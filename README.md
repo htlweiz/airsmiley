8936abee3666360ef5525cee3b0e9a7acf6bb37f
Christoph Wurzinger <wu@htlweiz.at>
2020-11-27 01:13:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# eCO2 measurement box
+A small-ish and simple box to measure
+- eCO2 with a CCS811
+- Temperature and Humidity with a DHT22
+using a Wemos D1 mini (or clone) with visualization on a
+- 0.96" I2C OLED
+- NeoPixel compatible RGB LED ring (WS2812B)
+
+The thresholds are set according to [REHVA's recommendations for schools](https://www.rehva.eu/fileadmin/user_upload/REHVA_COVID-19_guidance_document_School_guidance_25112020.pdf).
+
+## Burn-In
+For full sensor accuracy, according to the [datasheet](https://www.sciosense.com/wp-content/uploads/2020/01/CCS811-Datasheet.pdf), the device first needs a "burn-in" phase of 48 operating hours. 
+
+## Run-In
+After the "burn-in" phase, the values are considered accurate after a "run-in" time of 20 minutes after power on.
+
+## Flashing
+- Install the newest [ESP8266 support](https://www.heise.de/ct/artikel/Arduino-IDE-installieren-und-fit-machen-fuer-ESP8266-und-ESP32-4130814.html)
+- Add the following libraries (using the built-in library manager, *Tools –> Manage Libraries...*)
+  - NeoPixelBus by Makuna (LGPL-3.0 License)
+  - Adafruit CCS811 (MIT License)
+  - DHT sensor library by Adafruit (MIT License)
+  - Adafruit SSD1306 (BSD License)
+
+## PCB
+The PCB has been designed with KiCad and uses the corresponding [libraries](https://kicad.org/libraries/) which are available under the [Creative Commons CC-BY-SA 4.0 License](https://creativecommons.org/licenses/by-sa/4.0/legalcode). Additionally, a [footprint](https://github.com/rubienr/wemos-d1-mini-kicad) made by [rubienr](https://github.com/rubienr) is used.
+
+The PCB is designed to be screwed into a case available at [aliexpress](https://de.aliexpress.com/item/4000068796385.html?spm=a2g0s.9042311.0.0.52614c4dDeY8Wv).
---
+++ co2box.ino
@@ -0,0 +1,220 @@
+/*
+Copyright 2020 Christoph B. Wurzinger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+ 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "DHT.h"
+#include "Adafruit_CCS811.h"
+#include <NeoPixelBus.h>
+
+
+/***********************************************************************
+   CO2 Thresholds (ppm).
+***********************************************************************/
+// Recommendation from REHVA (Federation of European Heating, Ventilation and Air Conditioning associations, rehva.eu)
+// for preventing COVID-19 aerosol spread especially in schools:
+// - warn: 800 (yellow/orange), critical: 1000 (red)
+// (https://www.rehva.eu/fileadmin/user_upload/REHVA_COVID-19_guidance_document_School_guidance_25112020.pdf)
+
+#define CO2_WARN_PPM 800
+#define CO2_CRITICAL_PPM 1000
+
+
+
+/***********************************************************************
+   LED ring: NeoPixelBus Setup
+***********************************************************************/
+const uint16_t PixelCount = 8;
+const uint16_t colorSaturation = 10;
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> ring(PixelCount, -1);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor orange(colorSaturation, colorSaturation / 2, 0);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+
+/***********************************************************************
+   eCO2 sensor: CCS811 Setup
+***********************************************************************/
+Adafruit_CCS811 ccs;
+
+
+
+/***********************************************************************
+   Temperature and Humidity sensor: DHT22 Setup
+***********************************************************************/
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+/***********************************************************************
+   OLED display: SSD1306 Setup
+***********************************************************************/
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+
+/***********************************************************************
+   System Startup
+***********************************************************************/
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("\nStarting up"));
+
+
+  Serial.println(F("Bringing up eCO2 sensor"));
+  if (!ccs.begin(0x5a)) {
+    Serial.println("FAILED to start eCO2 sensor! Please check your wiring.");
+  }
+  ccs.setDriveMode(CCS811_DRIVE_MODE_1SEC);
+
+
+  Serial.println(F("Bringing up display"));
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("FAILED display allocation"));
+  }
+  display.setRotation(2);
+  display.clearDisplay();
+  display.display();
+
+
+  Serial.println(F("Bringing up LEDs"));
+  ring.Begin();
+  for (int i = 0; i < PixelCount; i ++) {
+    ring.SetPixelColor(i, white);
+  }
+  ring.Show();
+
+
+  Serial.println(F("Bringing up temperaturea and humidity sensor"));
+  dht.begin();
+
+
+  Serial.println(F("Setup complete."));
+}
+
+
+
+/***********************************************************************
+   Main Loop
+***********************************************************************/
+void loop() {
+  Serial.println(F("looping"));
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+
+  static float eco2 = 0;
+  static float tvoc = 0;
+
+  if (ccs.available()) {
+    ccs.setEnvironmentalData(humidity, temperature);
+    Serial.print(F("ccs.readData: ")); Serial.println(ccs.readData());
+    eco2 = ccs.geteCO2();
+    tvoc = ccs.getTVOC();
+  }
+
+  displayData(temperature,
+              humidity,
+              eco2,
+              tvoc);
+
+  delay(500);
+}
+
+
+/***********************************************************************
+   Visualize Data
+***********************************************************************/
+void displayData(float temp, float hum, uint32_t eco2, uint32_t tvoc) {
+  const int xshift = 6;
+  char *fills[] = {"    ", "   ", "  ", " ", ""};
+  char *temp_fill = fills[(int)log10(temp)];
+  char *hum_fill  = fills[(int)log10(hum)];
+  char *eco2_fill = fills[(int)log10(eco2)];
+  char *tvoc_fill = fills[(int)log10(tvoc)];
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(xshift, 0);
+  display.print(temp, 1); display.print((char)247); display.print(F("C "));
+  display.print(hum, 0); display.println(F("%"));
+  display.setTextSize(4);
+  display.setCursor(xshift, 16);
+  display.print(eco2_fill); display.print(eco2);
+  display.setCursor(xshift, 24);
+  display.setTextSize(1);
+  display.print(F("eCO2"));
+  display.setCursor(xshift, 32);
+  display.print(F("ppm"));
+
+  display.setCursor(xshift, 48);
+  display.setTextSize(1);
+  display.print(F("eTVOC"));
+  display.setCursor(xshift, 56);
+  display.print(F("ppb"));
+  display.setCursor(64, 48);
+  display.setTextSize(2);
+  display.print(tvoc_fill); display.print(tvoc);
+  display.display();
+
+
+  Serial.print(F("HTL Weiz "));
+  Serial.println(F("CO2box"));
+  Serial.print(F("T: ")); Serial.print(temp_fill); Serial.print(temp, 1); Serial.println(F("°C"));
+  Serial.print(F("H: ")); Serial.print(hum_fill); Serial.print(hum, 1); Serial.println(F("%"));
+  Serial.print(F("TVOC: ")); Serial.print(tvoc_fill); Serial.print(tvoc); Serial.println(F("ppb"));
+  Serial.print(F("eCO2: ")); Serial.print(eco2_fill); Serial.print(eco2); Serial.println(F("ppm"));
+
+
+  RgbColor color = green;
+  if (eco2 >= CO2_WARN_PPM) {
+    color = orange;
+    display.invertDisplay(true);
+  } else {
+    display.invertDisplay(false);
+  }
+  if (eco2 >= CO2_CRITICAL_PPM) {
+    color = red;
+  }
+
+  for (int i = 0; i < PixelCount; i ++) {
+    ring.SetPixelColor(i, color);
+  }
+  ring.Show();
+}
